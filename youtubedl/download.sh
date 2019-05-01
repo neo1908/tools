171771b4986f5bf8e13ca8b876475e7e1c2ef1a9
Ben Davies <daviesben7@gmail.com>
2019-05-02 00:43:19 +0100

    Added youtubedl to init repo

---
+++ youtubedl/download.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+OUTDIR="/mnt/gimli/Plex/Youtube/"
+#OUTDIR="/home/ben/youtubedl/Staging/"
+FORMAT=${OUTDIR}"%(uploader)s/%(title)s/%(uploader)s-%(title)s_%(upload_date)s_%(resolution)s.%(ext)s"
+ARCHIVE_FILE=${OUTDIR}downloads.lst
+OPTS="-a /home/ben/youtubedl/channels.lst --write-description --write-info-json --write-annotations --write-thumbnail --all-subs --audio-quality 0 -o ${FORMAT} --download-archive ${ARCHIVE_FILE}"
+#OPTS="-a /home/ben/youtubedl/channels.lst --write-info-json --write-annotations --write-thumbnail --all-subs --audio-quality 0 -o ${FORMAT} --download-archive ${ARCHIVE_FILE}"
+
+youtube-dl ${OPTS}
