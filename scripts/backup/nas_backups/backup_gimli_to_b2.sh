5c818c6021176bdfc066eedac593a0f0d3816dd5
Ben Davies <0x620x64@protonmail.com>
2021-02-05 18:36:33 +0000

    master - add backup scripts to be deployed w/ ansible

--- scripts/backup/nas_backups/backup_gimli_to_b2.sh
+++ scripts/backup/nas_backups/backup_gimli_to_b2.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-export B2_ACCOUNT_KEY=FROM_KEEPASS
-export B2_ACCOUNT_ID=FROM_KEEPASS
-export RESTIC_REPO=b2:bckblznasbkp02
+export B2_ACCOUNT_KEY="{{ b2_account_key }}"
+export B2_ACCOUNT_ID="{{ b2_account_id }}"
+export RESTIC_REPO="{{ b2_bucket_name }}"
 
 backup_result=$(restic backup -r $RESTIC_REPO --files-from /root/backups/.restic_conf/gimli_to_b2/gimli_to_b2.lst -p /root/backups/.restic_conf/gimli_to_b2/gimli_to_b2.pass)
 prune_result=$(restic forget --keep-within 1m --prune -r $RESTIC_REPO -p /root/backups/.restic_conf/gimli_to_b2/gimli_to_b2.pass)
@@ -13,7 +13,7 @@ ${backup_result}
 Forget and prune:
 
 ${prune_result}
-" | mail -s "Backup notification - Gimli :: ${RESTIC_REPO}" -r 'backup_alerts@'$(hostname) -S 'smtp=10.0.0.55:25' home_alerts@protonmail.com
+" | mail -s "Backup notification - Gimli :: ${RESTIC_REPO}" -r 'backup_alerts@'$(hostname) -S 'smtp={{ internal_smtp_ip }}:25' {{ alerts_email_address }}
 
 unset B2_ACCOUNT_KEY
 unset B2_ACCOUNT_ID
