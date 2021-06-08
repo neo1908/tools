1af2091f8d7363cde2e3b464b1e9f2cce2afdaa2
Ben Davies <0x620x64@protonmail.com>
2021-06-08 08:55:01 +0100

    Updated back script to exclude DNA

--- scripts/backup/nas_backups/backup_censt5secdev01p0_to_gimli.sh
+++ scripts/backup/nas_backups/backup_censt5secdev01p0_to_gimli.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
-backup_result=$(restic backup -r /mnt/gimli/Backup/censt5secdev01p0_restic -p /root/backups/.restic_conf/censt5secdev01p0/restic_censt5secdev01p0.pass --files-from /root/backups/.restic_conf/censt5secdev01p0/censt5secdev01p0_files.lst
-)
+backup_result=$(restic backup -r /mnt/gimli/Backup/censt5secdev01p0_restic -p /root/backups/.restic_conf/censt5secdev01p0/restic_censt5secdev01p0.pass --files-from /root/backups/.restic_conf/censt5secdev01p0/censt5secdev01p0_files.lst)
 
 prune_result=$(restic forget --keep-within 5d --prune -r /mnt/gimli/Backup/censt5secdev01p0_restic -p /root/backups/.restic_conf/censt5secdev01p0/restic_censt5secdev01p0.pass)
 
